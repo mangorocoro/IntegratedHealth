9d6f39655b1fc304f34d7e5348e239c2fa4f5189
Dcosiem <david.cosiem@yahoo.com>
2017-04-05 15:56:14 -0400

    Working table with clickable rows

--- app/src/main/java/com/example/dan/integratedhealth/DataEntryFragment.java
+++ app/src/main/java/com/example/dan/integratedhealth/DataEntryFragment.java
@@ -56,45 +56,45 @@ public class DataEntryFragment extends Fragment {
         wght_txt = (EditText) view.findViewById(R.id.Weight);
         rep_txt = (EditText) view.findViewById(R.id.Reps);
 
-        wght_minus = (ImageButton)  view.findViewById(R.id.weight_minus);
-        wght_minus.setOnClickListener(new View.OnClickListener(){
+        wght_minus = (ImageButton) view.findViewById(R.id.weight_minus);
+        wght_minus.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 String temp = wght_txt.getText().toString().trim();
-                if(temp.isEmpty()){
+                if (temp.isEmpty()) {
                     number_weight = 0;
                 } else {
                     number_weight = Float.parseFloat(wght_txt.getText().toString());
                 }
-                number_weight-=5;
+                number_weight -= 5;
                 wght_txt.setText(String.valueOf(number_weight));
             }
         });
 
-        wght_plus =  (ImageButton) view.findViewById(R.id.weight_plus);
-        wght_plus.setOnClickListener(new View.OnClickListener(){
+        wght_plus = (ImageButton) view.findViewById(R.id.weight_plus);
+        wght_plus.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 String temp = wght_txt.getText().toString().trim();
-                if(temp.isEmpty()){
+                if (temp.isEmpty()) {
                     number_weight = 0;
                 } else {
                     number_weight = Float.parseFloat(wght_txt.getText().toString());
                 }
-                number_weight+=5;
+                number_weight += 5;
                 wght_txt.setText(String.valueOf(number_weight));
             }
         });
 
-        rep_minus = (ImageButton)  view.findViewById(R.id.rep_minus);
-        rep_minus.setOnClickListener(new View.OnClickListener(){
+        rep_minus = (ImageButton) view.findViewById(R.id.rep_minus);
+        rep_minus.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 String temp = rep_txt.getText().toString().trim();
-                if(temp.isEmpty()){
+                if (temp.isEmpty()) {
                     number_rep = 0;
                 } else {
                     number_rep = Float.parseFloat(rep_txt.getText().toString());
@@ -104,13 +104,13 @@ public class DataEntryFragment extends Fragment {
             }
         });
 
-        rep_plus = (ImageButton)  view.findViewById(R.id.rep_plus);
-        rep_plus.setOnClickListener(new View.OnClickListener(){
+        rep_plus = (ImageButton) view.findViewById(R.id.rep_plus);
+        rep_plus.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 String temp = rep_txt.getText().toString().trim();
-                if(temp.isEmpty()){
+                if (temp.isEmpty()) {
                     number_rep = 0;
                 } else {
                     number_rep = Float.parseFloat(rep_txt.getText().toString());
@@ -121,11 +121,11 @@ public class DataEntryFragment extends Fragment {
         });
 
         clear = (Button) view.findViewById(R.id.clear);
-        clear.setOnClickListener(new View.OnClickListener(){
+        clear.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
 
-                if(clear.getText().equals("Delete")){
+                if (clear.getText().equals("Delete")) {
                     //change to delete function
                     CharSequence text = "Entry Deleted";
                     int duration = Toast.LENGTH_LONG;
@@ -140,7 +140,9 @@ public class DataEntryFragment extends Fragment {
                     save.setText("Save");
                     clear.setText("Clear");
                     l.setAdapter(adapter);
-                } else{
+
+
+                } else {
                     wght_txt.setText("");
                     rep_txt.setText("");
                 }
@@ -148,87 +150,90 @@ public class DataEntryFragment extends Fragment {
         });
 
         save = (Button) view.findViewById(R.id.save);
-        save.setOnClickListener(new View.OnClickListener(){
-           @Override
-           public void onClick(View v) {
-               String wght_temp = wght_txt.getText().toString().trim();
-               String rep_temp = rep_txt.getText().toString().trim();
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String wght_temp = wght_txt.getText().toString().trim();
+                String rep_temp = rep_txt.getText().toString().trim();
 
 
-               if (wght_temp.isEmpty() && rep_temp.isEmpty()) {
-                   CharSequence text = "Please enter a weight or number of reps";
-                   int duration = Toast.LENGTH_LONG;
-                   Toast toast = Toast.makeText(getContext(), text, duration);
-                   toast.show();
-               } else {
-                   if(save.getText().equals("Update")){
+                if (wght_temp.isEmpty() && rep_temp.isEmpty()) {
+                    CharSequence text = "Please enter a weight or number of reps";
+                    int duration = Toast.LENGTH_LONG;
+                    Toast toast = Toast.makeText(getContext(), text, duration);
+                    toast.show();
+                } else {
+                    if (save.getText().equals("Update")) {
                         //change to update function
-                       CharSequence text = "Updated";
-                       int duration = Toast.LENGTH_LONG;
-                       Toast toast = Toast.makeText(getContext(), text, duration);
-                       toast.show();
+                        CharSequence text = "Updated";
+                        int duration = Toast.LENGTH_LONG;
+                        Toast toast = Toast.makeText(getContext(), text, duration);
+                        toast.show();
 
-                       float weight_key = Float.parseFloat(wght_temp);
-                       float rep_value = Float.parseFloat(rep_temp);
-                       String entry = wght_temp + " lbs                         ";
-                       entry += rep_temp + " reps";
+                        float weight_key = Float.parseFloat(wght_temp);
+                        float rep_value = Float.parseFloat(rep_temp);
+                        String entry = wght_temp + " lbs                         ";
+                        entry += rep_temp + " reps";
 
-                       final ArrayAdapter<String> adapter;
-                       adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, results);
-                       final ListView l = (ListView) view.findViewById(R.id.list_results);
-                       results.set(selected, entry);
-                       TextView x = (TextView) l.getChildAt(selected);
-                       x.setText(entry);
-                       adapter.notifyDataSetChanged();
+                        final ArrayAdapter<String> adapter;
+                        adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, results);
+                        final ListView l = (ListView) view.findViewById(R.id.list_results);
+                        results.set(selected, entry);
+                        TextView x = (TextView) l.getChildAt(selected);
+                        x.setText(entry);
+                        adapter.notifyDataSetChanged();
 
-                       save.setText("Save");
-                       clear.setText("Clear");
-                       l.setAdapter(adapter);
-
-                   } else {
-                       CharSequence text = "Saved";
-                       int duration = Toast.LENGTH_LONG;
-                       Toast toast = Toast.makeText(getContext(), text, duration);
-                       toast.show();
-
-                       final ArrayAdapter<String> adapter;
-
-                       float weight_key = Float.parseFloat(wght_temp);
-                       float rep_value = Float.parseFloat(rep_temp);
-                       String entry = wght_temp + " lbs                         ";
-                       entry += rep_temp + " reps";
-                       results.add(entry);
-                       adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, results);
-                       final ListView listView = (ListView) view.findViewById(R.id.list_results);
-
-                       listView.setAdapter(adapter);
-                       listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-                           @Override
-                           public void onItemClick(AdapterView<?> parent, View v1, int position, long id) {
-                               TextView w = (TextView) listView.getChildAt(position);
-                               Matcher m = Pattern.compile("\\d+\\.\\d+").matcher(w.getText().toString());
-                               int counter = 0;
-                               while(m.find()){
-                                   if(counter == 0){
-                                       wght_txt.setText(m.group(0));
-                                   } else {
-                                       rep_txt.setText(m.group(0));
-                                   }
-                                   counter++;
-                               }
-                               update = (Button) view.findViewById(R.id.save);
-                               update.setText("Update");
-                               delete = (Button) view.findViewById(R.id.clear);
-                               delete.setText("Delete");
-                               selected = position;
-                           }
-                       });
-                   }
+                        save.setText("Save");
+                        clear.setText("Clear");
+                        l.setAdapter(adapter);
 
 
-               }
-           }
+
+                    } else {
+                        CharSequence text = "Saved";
+                        int duration = Toast.LENGTH_LONG;
+                        Toast toast = Toast.makeText(getContext(), text, duration);
+                        toast.show();
+
+                        final ArrayAdapter<String> adapter;
+
+                        float weight_key = Float.parseFloat(wght_temp);
+                        float rep_value = Float.parseFloat(rep_temp);
+                        String entry = wght_temp + " lbs                         ";
+                        entry += rep_temp + " reps";
+                        results.add(entry);
+
+                        adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, results);
+                        final ListView listView = (ListView) view.findViewById(R.id.list_results);
+
+                        listView.setAdapter(adapter);
+                        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View v1, int position, long id) {
+                                TextView w = (TextView) listView.getChildAt(position);
+                                Matcher m = Pattern.compile("\\d+\\.\\d+").matcher(w.getText().toString());
+                                int counter = 0;
+                                while (m.find()) {
+                                    if (counter == 0) {
+                                        wght_txt.setText(m.group(0));
+                                    } else {
+                                        rep_txt.setText(m.group(0));
+                                    }
+                                    counter++;
+                                }
+                                update = (Button) view.findViewById(R.id.save);
+                                update.setText("Update");
+                                delete = (Button) view.findViewById(R.id.clear);
+                                delete.setText("Delete");
+                                selected = position;
+                            }
+                        });
+                    }
+
+
+                }
+            }
 
         });
         return view;
