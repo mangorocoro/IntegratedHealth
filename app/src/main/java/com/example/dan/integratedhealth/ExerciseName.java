d0a668fd7e5a8cbe1faf933f6a2da0b6078682e8
Dcosiem <david.cosiem@yahoo.com>
2017-04-06 01:58:27 -0400

    Working table with clickable rows

---
+++ app/src/main/java/com/example/dan/integratedhealth/ExerciseName.java
@@ -0,0 +1,19 @@
+package com.example.dan.integratedhealth;
+
+/**
+ * Created by Vit on 4/5/2017.
+ */
+
+public class ExerciseName {
+
+    public String exercise_name;
+
+    public ExerciseName(){
+        this.exercise_name = "";
+    }
+    public ExerciseName(String text){
+        this.exercise_name = text;
+    }
+
+
+}
