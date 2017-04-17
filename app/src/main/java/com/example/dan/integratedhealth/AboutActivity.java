2d7bf0c4b9833a6c467e5a49cb5192e58c5ff7cf
mangorocoro <daniel01001000@gmail.com>
2017-04-17 01:38:55 -0400

    added about listView functionality

--- app/src/main/java/com/example/dan/integratedhealth/AboutActivity.java
+++ app/src/main/java/com/example/dan/integratedhealth/AboutActivity.java
@@ -1,13 +1,50 @@
 package com.example.dan.integratedhealth;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class AboutActivity extends AppCompatActivity {
+public class AboutActivity extends Activity {
+
+
+    ListView list;
+    String[] itemname ={
+            "General",
+            "Fitness",
+            "Diet"
+    };
+
+    Integer[] imgid={
+            R.drawable.scale,
+            R.drawable.runner,
+            R.drawable.applebanana
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        CustomListAdapter adapter=new CustomListAdapter(this, itemname, imgid);
+        list=(ListView)findViewById(R.id.list);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // TODO Auto-generated method stub
+                String Slecteditem= itemname[+position];
+                Toast.makeText(getApplicationContext(), Slecteditem, Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
